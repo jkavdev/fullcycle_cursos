9072ee6bfd773834ed810488f3892f56a18f314c
JHONATAN KOLEN ALVES VIANA <f930649@corp.caixa.gov.br>
2023-01-28 02:43:49 -0300

    Full Cycle 3.0 - Projeto prático - Java (Back-end) - 0304 - criando entidade category com tdd

--- Full Cycle 3.0 - Projeto prático - Java ( Back-end )/fc3-admin-do-catalago/infrastructure/src/main/java/br/com/jkavdev/fullcycle/admin/catalogo/infrastructure/Main.java
+++
@@ -1,9 +0,0 @@
-package br.com.jkavdev.fullcycle.admin.catalogo.infrastructure;
-
-import br.com.jkavdev.fullcycle.admin.catalogo.application.UseCase;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println(new UseCase().execute());
-    }
-}
