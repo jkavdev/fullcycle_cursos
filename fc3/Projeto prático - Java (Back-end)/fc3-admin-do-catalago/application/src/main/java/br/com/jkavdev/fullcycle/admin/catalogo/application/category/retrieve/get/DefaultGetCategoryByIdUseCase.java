b27896a38ee59db20cdd6d0db3fde139c2fd2582
JHONATAN KOLEN ALVES VIANA <f930649@corp.caixa.gov.br>
2023-01-31 01:28:40 -0300

    Full Cycle 3.0 - Projeto prático - Java (Back-end) - 0418 - implementando o caso de uso de buscar por id a categoria

---
+++ fc3/Projeto prático - Java (Back-end)/fc3-admin-do-catalago/application/src/main/java/br/com/jkavdev/fullcycle/admin/catalogo/application/category/retrieve/get/DefaultGetCategoryByIdUseCase.java
@@ -0,0 +1,32 @@
+package br.com.jkavdev.fullcycle.admin.catalogo.application.category.retrieve.get;
+
+import br.com.jkavdev.fullcycle.admin.catalogo.domain.category.CategoryGateway;
+import br.com.jkavdev.fullcycle.admin.catalogo.domain.category.CategoryID;
+import br.com.jkavdev.fullcycle.admin.catalogo.domain.exceptions.DomainException;
+import br.com.jkavdev.fullcycle.admin.catalogo.domain.validation.Error;
+
+import java.util.Objects;
+import java.util.function.Supplier;
+
+public class DefaultGetCategoryByIdUseCase extends GetCategoryByIdUseCase {
+
+    private final CategoryGateway categoryGateway;
+
+    public DefaultGetCategoryByIdUseCase(final CategoryGateway categoryGateway) {
+        this.categoryGateway = Objects.requireNonNull(categoryGateway);
+    }
+
+    @Override
+    public CategoryOutput execute(String anIn) {
+        CategoryID aCategoryId = CategoryID.from(anIn);
+        return this.categoryGateway.findById(aCategoryId)
+                .map(CategoryOutput::from)
+                .orElseThrow(notFound(aCategoryId));
+    }
+
+    private Supplier<DomainException> notFound(final CategoryID anId) {
+        return () -> DomainException.with(
+                new Error("Category with ID %s was not found".formatted(anId.getValue()))
+        );
+    }
+}
