b0cc6a5cbf9aa5f11f4b2f99f728afc2f616adb8
JHONATAN KOLEN ALVES VIANA <f930649@corp.caixa.gov.br>
2023-01-31 16:32:19 -0300

    Full Cycle 3.0 - Projeto prático - Java (Back-end) - 0503 - configurando profiles do spring boot

--- fc3/Projeto prático - Java (Back-end)/fc3-admin-do-catalago/infrastructure/src/main/java/br/com/jkavdev/fullcycle/admin/catalogo/infrastructure/Main.java
+++ fc3/Projeto prático - Java (Back-end)/fc3-admin-do-catalago/infrastructure/src/main/java/br/com/jkavdev/fullcycle/admin/catalogo/infrastructure/Main.java
@@ -3,10 +3,13 @@ package br.com.jkavdev.fullcycle.admin.catalogo.infrastructure;
 import br.com.jkavdev.fullcycle.admin.catalogo.infrastructure.configuration.WebserverConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.AbstractEnvironment;
 
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
+        System.setProperty(AbstractEnvironment.DEFAULT_PROFILES_PROPERTY_NAME, "development");
         SpringApplication.run(WebserverConfig.class, args);
+
     }
 }
