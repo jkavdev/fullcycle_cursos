ea4933b204d3946b46dc4290960abb4ebaae3366
JHONATAN KOLEN ALVES VIANA <f930649@corp.caixa.gov.br>
2023-02-03 02:26:34 -0300

    Full Cycle 3.0 - Projeto prático - Java (Back-end) - 0806 - implementando o endpoint de criacao de categoria

--- fc3/Projeto prático - Java (Back-end)/fc3-admin-do-catalago/infrastructure/src/main/java/br/com/jkavdev/fullcycle/admin/catalogo/infrastructure/api/controllers/CategoryController.java
+++ fc3/Projeto prático - Java (Back-end)/fc3-admin-do-catalago/infrastructure/src/main/java/br/com/jkavdev/fullcycle/admin/catalogo/infrastructure/api/controllers/CategoryController.java
@@ -1,11 +1,17 @@
 package br.com.jkavdev.fullcycle.admin.catalogo.infrastructure.api.controllers;
 
+import br.com.jkavdev.fullcycle.admin.catalogo.application.category.create.CreateCategoryCommand;
+import br.com.jkavdev.fullcycle.admin.catalogo.application.category.create.CreateCategoryOutput;
 import br.com.jkavdev.fullcycle.admin.catalogo.application.category.create.CreateCategoryUseCase;
 import br.com.jkavdev.fullcycle.admin.catalogo.domain.pagination.Pagination;
+import br.com.jkavdev.fullcycle.admin.catalogo.domain.validation.handler.Notification;
+import br.com.jkavdev.fullcycle.admin.catalogo.infrastructure.category.models.CreateCategoryApiInput;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.URI;
 import java.util.Objects;
+import java.util.function.Function;
 
 @RestController
 public class CategoryController implements CategoryAPI {
@@ -17,8 +23,23 @@ public class CategoryController implements CategoryAPI {
     }
 
     @Override
-    public ResponseEntity<?> createCategory() {
-        return null;
+    public ResponseEntity<?> createCategory(final CreateCategoryApiInput anInput) {
+        final var aCommand = CreateCategoryCommand.with(
+                anInput.name(),
+                anInput.description(),
+                anInput.active().booleanValue()
+        );
+
+        final Function<Notification, ResponseEntity<?>> onError = notification ->
+                ResponseEntity.unprocessableEntity().body(notification);
+
+        final Function<CreateCategoryOutput, ResponseEntity<?>> onSuccess = output ->
+                ResponseEntity
+                        .created(URI.create("/categories/" + output.id()))
+                        .body(output);
+
+        return this.createCategoryUseCase.execute(aCommand)
+                .fold(onError, onSuccess);
     }
 
     @Override
--- fc3/Projeto prático - Java (Back-end)/fc3-admin-do-catalago/infrastructure/src/test/java/br/com/jkavdev/fullcycle/admin/catalogo/infrastructure/api/CategoryAPITest.java
+++ fc3/Projeto prático - Java (Back-end)/fc3-admin-do-catalago/infrastructure/src/test/java/br/com/jkavdev/fullcycle/admin/catalogo/infrastructure/api/CategoryAPITest.java
@@ -1,11 +1,26 @@
 package br.com.jkavdev.fullcycle.admin.catalogo.infrastructure.api;
 
 import br.com.jkavdev.fullcycle.admin.catalogo.ControllerTest;
+import br.com.jkavdev.fullcycle.admin.catalogo.application.category.create.CreateCategoryOutput;
 import br.com.jkavdev.fullcycle.admin.catalogo.application.category.create.CreateCategoryUseCase;
+import br.com.jkavdev.fullcycle.admin.catalogo.domain.category.CategoryID;
+import br.com.jkavdev.fullcycle.admin.catalogo.infrastructure.category.models.CreateCategoryApiInput;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.Objects;
+
+import static io.vavr.API.Right;
+import static org.hamcrest.Matchers.equalTo;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @ControllerTest
 public class CategoryAPITest {
@@ -13,11 +28,41 @@ public class CategoryAPITest {
     @Autowired
     private MockMvc mvc;
 
+    @Autowired
+    private ObjectMapper mapper;
+
     @MockBean
     private CreateCategoryUseCase createCategoryUseCase;
 
     @Test
-    public void test() {
+    public void givenAValidCommand_whenCallsCreateCategory_shouldReturnCategoryId() throws Exception {
+
+        final var expectedName = "Filmes";
+        final var expectedDescription = "Categoria de filmes";
+        final var expectedIsActive = true;
+
+        final var anInput =
+                new CreateCategoryApiInput(expectedName, expectedDescription, expectedIsActive);
+
+        when(createCategoryUseCase.execute(any()))
+                .thenReturn(Right(CreateCategoryOutput.from(CategoryID.from("123"))));
+
+        final var request = post("/categories")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(mapper.writeValueAsString(anInput));
+
+        this.mvc.perform(request)
+                .andDo(print())
+                .andExpect(status().isCreated())
+                .andExpect(header().string("Location", "/categories/123"))
+                .andExpect(header().string("Content-Type", MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(jsonPath("$.id", equalTo("123")));
+
+        verify(createCategoryUseCase, times(1)).execute(argThat(cmd ->
+                Objects.equals(expectedName, cmd.name())
+                        && Objects.equals(expectedDescription, cmd.description())
+                        && Objects.equals(expectedIsActive, cmd.isActive())
+        ));
 
     }
 
