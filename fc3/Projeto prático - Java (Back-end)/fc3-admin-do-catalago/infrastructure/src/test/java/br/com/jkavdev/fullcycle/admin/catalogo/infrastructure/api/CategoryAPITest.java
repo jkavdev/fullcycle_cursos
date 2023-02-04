d3e7bdc6df7292d4c51b058f4ddfd3c39c74aefe
JHONATAN KOLEN ALVES VIANA <f930649@corp.caixa.gov.br>
2023-02-04 00:30:36 -0300

    Full Cycle 3.0 - Projeto prático - Java (Back-end) - 0807 -implementando handler de erro global

--- fc3/Projeto prático - Java (Back-end)/fc3-admin-do-catalago/infrastructure/src/test/java/br/com/jkavdev/fullcycle/admin/catalogo/infrastructure/api/CategoryAPITest.java
+++ fc3/Projeto prático - Java (Back-end)/fc3-admin-do-catalago/infrastructure/src/test/java/br/com/jkavdev/fullcycle/admin/catalogo/infrastructure/api/CategoryAPITest.java
@@ -4,6 +4,9 @@ import br.com.jkavdev.fullcycle.admin.catalogo.ControllerTest;
 import br.com.jkavdev.fullcycle.admin.catalogo.application.category.create.CreateCategoryOutput;
 import br.com.jkavdev.fullcycle.admin.catalogo.application.category.create.CreateCategoryUseCase;
 import br.com.jkavdev.fullcycle.admin.catalogo.domain.category.CategoryID;
+import br.com.jkavdev.fullcycle.admin.catalogo.domain.exceptions.DomainException;
+import br.com.jkavdev.fullcycle.admin.catalogo.domain.validation.Error;
+import br.com.jkavdev.fullcycle.admin.catalogo.domain.validation.handler.Notification;
 import br.com.jkavdev.fullcycle.admin.catalogo.infrastructure.category.models.CreateCategoryApiInput;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
@@ -14,8 +17,9 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.Objects;
 
+import static io.vavr.API.Left;
 import static io.vavr.API.Right;
-import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -57,6 +61,75 @@ public class CategoryAPITest {
                 .andExpect(header().string("Location", "/categories/123"))
                 .andExpect(header().string("Content-Type", MediaType.APPLICATION_JSON_VALUE))
                 .andExpect(jsonPath("$.id", equalTo("123")));
+
+        verify(createCategoryUseCase, times(1)).execute(argThat(cmd ->
+                Objects.equals(expectedName, cmd.name())
+                        && Objects.equals(expectedDescription, cmd.description())
+                        && Objects.equals(expectedIsActive, cmd.isActive())
+        ));
+
+    }
+
+    @Test
+    public void givenAInvalidName_whenCallsCreateCategory_thenShouldReturnNotification() throws Exception {
+
+        final var expectedName = "Filmes";
+        final var expectedDescription = "Categoria de filmes";
+        final var expectedIsActive = true;
+        final var expectedErrorMessage = "'name' should not be null";
+
+        final var anInput =
+                new CreateCategoryApiInput(expectedName, expectedDescription, expectedIsActive);
+
+        when(createCategoryUseCase.execute(any()))
+                .thenReturn(Left(Notification.create(new Error(expectedErrorMessage))));
+
+        final var request = post("/categories")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(mapper.writeValueAsString(anInput));
+
+        this.mvc.perform(request)
+                .andDo(print())
+                .andExpect(status().isUnprocessableEntity())
+                .andExpect(header().string("Location", nullValue()))
+                .andExpect(header().string("Content-Type", MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(jsonPath("$.errors", hasSize(1)))
+                .andExpect(jsonPath("$.errors[0].message", equalTo(expectedErrorMessage)));
+
+        verify(createCategoryUseCase, times(1)).execute(argThat(cmd ->
+                Objects.equals(expectedName, cmd.name())
+                        && Objects.equals(expectedDescription, cmd.description())
+                        && Objects.equals(expectedIsActive, cmd.isActive())
+        ));
+
+    }
+
+    @Test
+    public void givenAInvalidCommand_whenCallsCreateCategory_thenShouldReturnDomainException() throws Exception {
+
+        final var expectedName = "Filmes";
+        final var expectedDescription = "Categoria de filmes";
+        final var expectedIsActive = true;
+        final var expectedErrorMessage = "'name' should not be null";
+
+        final var anInput =
+                new CreateCategoryApiInput(expectedName, expectedDescription, expectedIsActive);
+
+        when(createCategoryUseCase.execute(any()))
+                .thenThrow((DomainException.with(new Error(expectedErrorMessage))));
+
+        final var request = post("/categories")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(mapper.writeValueAsString(anInput));
+
+        this.mvc.perform(request)
+                .andDo(print())
+                .andExpect(status().isUnprocessableEntity())
+                .andExpect(header().string("Location", nullValue()))
+                .andExpect(header().string("Content-Type", MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(jsonPath("$.errors", hasSize(1)))
+                .andExpect(jsonPath("$.message", equalTo(expectedErrorMessage)))
+                .andExpect(jsonPath("$.errors[0].message", equalTo(expectedErrorMessage)));
 
         verify(createCategoryUseCase, times(1)).execute(argThat(cmd ->
                 Objects.equals(expectedName, cmd.name())
