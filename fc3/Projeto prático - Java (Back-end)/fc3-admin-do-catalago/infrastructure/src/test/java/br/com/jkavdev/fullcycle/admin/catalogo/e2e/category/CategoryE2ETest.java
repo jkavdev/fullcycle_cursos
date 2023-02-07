4201c1e1f705fcd9ee984afb9ea108d0a45d0294
JHONATAN KOLEN ALVES VIANA <f930649@corp.caixa.gov.br>
2023-02-07 01:46:45 -0300

    Full Cycle 3.0 - Projeto prático - Java (Back-end) - 0904 - testes e2e get de categoria por id

--- fc3/Projeto prático - Java (Back-end)/fc3-admin-do-catalago/infrastructure/src/test/java/br/com/jkavdev/fullcycle/admin/catalogo/e2e/category/CategoryE2ETest.java
+++ fc3/Projeto prático - Java (Back-end)/fc3-admin-do-catalago/infrastructure/src/test/java/br/com/jkavdev/fullcycle/admin/catalogo/e2e/category/CategoryE2ETest.java
@@ -149,6 +149,41 @@ public class CategoryE2ETest {
                 .andExpect(jsonPath("$.items[2].name", equalTo("Séries")));
     }
 
+    @Test
+    public void asACatalogAdminIShouldBeAbleToGetACategoryByItsIdentifier() throws Exception {
+        Assertions.assertTrue(MYSQL_CONTAINER.isRunning());
+        Assertions.assertEquals(0, categoryRepository.count());
+
+        final var expectedName = "Filmes";
+        final var expectedDescription = "A categoria mais assistida";
+        final var expectedIsActive = true;
+
+        final var actualId = givenACategory(expectedName, expectedDescription, expectedIsActive);
+
+        final var actualCategory = retrieveACategory(actualId.getValue());
+
+        Assertions.assertEquals(expectedName, actualCategory.name());
+        Assertions.assertEquals(expectedDescription, actualCategory.description());
+        Assertions.assertEquals(expectedIsActive, actualCategory.active());
+        Assertions.assertNotNull(actualCategory.createdAt());
+        Assertions.assertNotNull(actualCategory.updatedAt());
+        Assertions.assertNull(actualCategory.deletedAt());
+    }
+
+    @Test
+    public void asACatalogAdminIShouldBeAbleToSeeATreatedErrorByGettingANotFoundCategory() throws Exception {
+        Assertions.assertTrue(MYSQL_CONTAINER.isRunning());
+        Assertions.assertEquals(0, categoryRepository.count());
+
+        final var aRequest = get("/categories/123")
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON);
+
+        this.mvc.perform(aRequest)
+                .andExpect(status().isNotFound())
+                .andExpect(jsonPath("$.message", equalTo("Category with ID 123 was not found")));
+    }
+
     private CategoryID givenACategory(
             final String aName,
             final String aDescription,
@@ -168,7 +203,7 @@ public class CategoryE2ETest {
     }
 
     private CategoryResponse retrieveACategory(final String anId) throws Exception {
-        final var aRequest = get("/categories", anId);
+        final var aRequest = get("/categories/" + anId);
 
         final var json = this.mvc.perform(aRequest)
                 .andDo(print())
