6d5430a371fb7659fb72d3215bb67e637b640ad7
JHONATAN KOLEN ALVES VIANA <f930649@corp.caixa.gov.br>
2023-02-11 20:37:27 -0300

    Full Cycle 3.0 - Projeto prático - Java (Back-end) - 1205 - implementando endpoint de get genero por id

--- fc3/Projeto prático - Java (Back-end)/fc3-admin-do-catalago/infrastructure/src/main/java/br/com/jkavdev/fullcycle/admin/catalogo/infrastructure/api/controllers/GenreController.java
+++ fc3/Projeto prático - Java (Back-end)/fc3-admin-do-catalago/infrastructure/src/main/java/br/com/jkavdev/fullcycle/admin/catalogo/infrastructure/api/controllers/GenreController.java
@@ -2,11 +2,13 @@ package br.com.jkavdev.fullcycle.admin.catalogo.infrastructure.api.controllers;
 
 import br.com.jkavdev.fullcycle.admin.catalogo.application.genre.create.CreateGenreCommand;
 import br.com.jkavdev.fullcycle.admin.catalogo.application.genre.create.CreateGenreUseCase;
+import br.com.jkavdev.fullcycle.admin.catalogo.application.genre.retrieve.get.GetGenreByIdUseCase;
 import br.com.jkavdev.fullcycle.admin.catalogo.domain.pagination.Pagination;
 import br.com.jkavdev.fullcycle.admin.catalogo.infrastructure.genre.models.CreateGenreRequest;
 import br.com.jkavdev.fullcycle.admin.catalogo.infrastructure.genre.models.GenreListResponse;
 import br.com.jkavdev.fullcycle.admin.catalogo.infrastructure.genre.models.GenreResponse;
 import br.com.jkavdev.fullcycle.admin.catalogo.infrastructure.genre.models.UpdateGenreRequest;
+import br.com.jkavdev.fullcycle.admin.catalogo.infrastructure.genre.presenters.GenreApiPresenter;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,17 +20,21 @@ public class GenreController implements GenreAPI {
 
     final private CreateGenreUseCase createGenreUseCase;
 
+    final private GetGenreByIdUseCase getGenreByIdUseCase;
+
     public GenreController(
-            final CreateGenreUseCase createGenreUseCase
+            final CreateGenreUseCase createGenreUseCase,
+            final GetGenreByIdUseCase getGenreByIdUseCase
     ) {
         this.createGenreUseCase = Objects.requireNonNull(createGenreUseCase);
+        this.getGenreByIdUseCase = Objects.requireNonNull(getGenreByIdUseCase);
     }
 
     @Override
     public ResponseEntity<?> createGenre(final CreateGenreRequest anInput) {
         final var aCommand = CreateGenreCommand.with(
                 anInput.name(),
-                anInput.active(),
+                anInput.isActive(),
                 anInput.categories()
         );
         final var output = createGenreUseCase.execute(aCommand);
@@ -48,7 +54,7 @@ public class GenreController implements GenreAPI {
 
     @Override
     public GenreResponse getById(final String id) {
-        return null;
+        return GenreApiPresenter.present(this.getGenreByIdUseCase.execute(id));
     }
 
     @Override
--- fc3/Projeto prático - Java (Back-end)/fc3-admin-do-catalago/infrastructure/src/test/java/br/com/jkavdev/fullcycle/admin/catalogo/infrastructure/api/GenreAPITest.java
+++ fc3/Projeto prático - Java (Back-end)/fc3-admin-do-catalago/infrastructure/src/test/java/br/com/jkavdev/fullcycle/admin/catalogo/infrastructure/api/GenreAPITest.java
@@ -3,7 +3,13 @@ package br.com.jkavdev.fullcycle.admin.catalogo.infrastructure.api;
 import br.com.jkavdev.fullcycle.admin.catalogo.ControllerTest;
 import br.com.jkavdev.fullcycle.admin.catalogo.application.genre.create.CreateGenreOutput;
 import br.com.jkavdev.fullcycle.admin.catalogo.application.genre.create.CreateGenreUseCase;
+import br.com.jkavdev.fullcycle.admin.catalogo.application.genre.retrieve.get.GenreOutput;
+import br.com.jkavdev.fullcycle.admin.catalogo.application.genre.retrieve.get.GetGenreByIdUseCase;
+import br.com.jkavdev.fullcycle.admin.catalogo.domain.category.CategoryID;
+import br.com.jkavdev.fullcycle.admin.catalogo.domain.exceptions.NotFoundException;
 import br.com.jkavdev.fullcycle.admin.catalogo.domain.exceptions.NotificationException;
+import br.com.jkavdev.fullcycle.admin.catalogo.domain.genre.Genre;
+import br.com.jkavdev.fullcycle.admin.catalogo.domain.genre.GenreID;
 import br.com.jkavdev.fullcycle.admin.catalogo.domain.validation.Error;
 import br.com.jkavdev.fullcycle.admin.catalogo.domain.validation.handler.Notification;
 import br.com.jkavdev.fullcycle.admin.catalogo.infrastructure.api.controllers.GenreAPI;
@@ -21,6 +27,7 @@ import java.util.Objects;
 import static org.hamcrest.Matchers.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -36,6 +43,9 @@ public class GenreAPITest {
 
     @MockBean
     private CreateGenreUseCase createGenreUseCase;
+
+    @MockBean
+    private GetGenreByIdUseCase getGenreByIdUseCase;
 
     @Test
     public void givenAValidCommand_whenCallsCreateGenre_shouldReturnGenreId() throws Exception {
@@ -106,6 +116,70 @@ public class GenreAPITest {
                         && Objects.equals(expectedCategories, cmd.categories())
                         && Objects.equals(expectedIsActive, cmd.isActive())
         ));
+    }
+
+    @Test
+    public void givenAValidId_whenCallsGetGenreById_shouldReturnGenre() throws Exception {
+        // given
+        final var expectedName = "Ação";
+        final var expectedCategories = List.of("123", "456");
+        final var expectedIsActive = false;
+
+        final var aGenre = Genre.newGenre(expectedName, expectedIsActive)
+                .addCategories(
+                        expectedCategories.stream()
+                                .map(CategoryID::from)
+                                .toList()
+                );
+
+        final var expectedId = aGenre.getId().getValue();
+
+        when(getGenreByIdUseCase.execute(any()))
+                .thenReturn(GenreOutput.from(aGenre));
+
+        // when
+        final var aRequest = get("/genres/{id}", expectedId)
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON);
+
+        final var response = this.mvc.perform(aRequest);
+
+        // then
+        response.andExpect(status().isOk())
+                .andExpect(header().string("Content-Type", MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(jsonPath("$.id", equalTo(expectedId)))
+                .andExpect(jsonPath("$.name", equalTo(expectedName)))
+                .andExpect(jsonPath("$.categories_id", equalTo(expectedCategories)))
+                .andExpect(jsonPath("$.is_active", equalTo(expectedIsActive)))
+                .andExpect(jsonPath("$.created_at", equalTo(aGenre.getCreatedAt().toString())))
+                .andExpect(jsonPath("$.updated_at", equalTo(aGenre.getUpdatedAt().toString())))
+                .andExpect(jsonPath("$.deleted_at", equalTo(aGenre.getDeletedAt().toString())));
+
+        verify(getGenreByIdUseCase).execute(eq(expectedId));
+    }
+
+    @Test
+    public void givenAnInvalidId_whenCallsGetGenreById_shouldReturnNotFound() throws Exception {
+        // given
+        final var expectedErrorMessage = "Genre with ID 123 was not found";
+        final var expectedId = GenreID.from("123");
+
+        when(getGenreByIdUseCase.execute(any()))
+                .thenThrow(NotFoundException.with(Genre.class, expectedId));
+
+        // when
+        final var aRequest = get("/genres/{id}", expectedId.getValue())
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON);
+
+        final var response = this.mvc.perform(aRequest);
+
+        // then
+        response.andExpect(status().isNotFound())
+                .andExpect(header().string("Content-Type", MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(jsonPath("$.message", equalTo(expectedErrorMessage)));
+
+        verify(getGenreByIdUseCase).execute(eq(expectedId.getValue()));
     }
 
 }
