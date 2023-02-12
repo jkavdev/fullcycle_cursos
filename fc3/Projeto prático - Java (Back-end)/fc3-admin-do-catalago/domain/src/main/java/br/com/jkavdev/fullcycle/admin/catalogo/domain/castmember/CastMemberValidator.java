a16326b76b047f699fcf3199c8d7c19f59bbbecf
JHONATAN KOLEN ALVES VIANA <f930649@corp.caixa.gov.br>
2023-02-12 01:19:29 -0300

    Full Cycle 3.0 - Projeto prático - Java (Back-end) - 1405 - implementando o castmember validator

--- fc3/Projeto prático - Java (Back-end)/fc3-admin-do-catalago/domain/src/main/java/br/com/jkavdev/fullcycle/admin/catalogo/domain/castmember/CastMemberValidator.java
+++ fc3/Projeto prático - Java (Back-end)/fc3-admin-do-catalago/domain/src/main/java/br/com/jkavdev/fullcycle/admin/catalogo/domain/castmember/CastMemberValidator.java
@@ -1,9 +1,13 @@
 package br.com.jkavdev.fullcycle.admin.catalogo.domain.castmember;
 
+import br.com.jkavdev.fullcycle.admin.catalogo.domain.validation.Error;
 import br.com.jkavdev.fullcycle.admin.catalogo.domain.validation.ValidationHandler;
 import br.com.jkavdev.fullcycle.admin.catalogo.domain.validation.Validator;
 
 public class CastMemberValidator extends Validator {
+
+    public static final int NAME_MIN_LENGTH = 3;
+    public static final int NAME_MAX_LENGTH = 255;
 
     private final CastMember aMember;
 
@@ -14,7 +18,33 @@ public class CastMemberValidator extends Validator {
 
     @Override
     public void validate() {
+        checkNameConstraints();
+        checkTypeConstraints();
+    }
 
+    private void checkNameConstraints() {
+        final var name = this.aMember.getName();
+        if (name == null) {
+            this.validationHandler().append(new Error("'name' should not be null"));
+            return;
+        }
+
+        if (name.isBlank()) {
+            this.validationHandler().append(new Error("'name' should not be empty"));
+            return;
+        }
+
+        final var nameLength = name.trim().length();
+        if (nameLength < NAME_MIN_LENGTH || nameLength > NAME_MAX_LENGTH) {
+            this.validationHandler().append(new Error("'name' must be between 3 and 255 characters"));
+        }
+    }
+
+    private void checkTypeConstraints() {
+        final var type = this.aMember.getType();
+        if (type == null) {
+            this.validationHandler().append(new Error("'type' should not be null"));
+        }
     }
 
 
--- fc3/Projeto prático - Java (Back-end)/fc3-admin-do-catalago/domain/src/test/java/br/com/jkavdev/fullcycle/admin/catalogo/domain/castmember/CastMemberTest.java
+++ fc3/Projeto prático - Java (Back-end)/fc3-admin-do-catalago/domain/src/test/java/br/com/jkavdev/fullcycle/admin/catalogo/domain/castmember/CastMemberTest.java
@@ -1,5 +1,6 @@
 package br.com.jkavdev.fullcycle.admin.catalogo.domain.castmember;
 
+import br.com.jkavdev.fullcycle.admin.catalogo.domain.exceptions.NotificationException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -19,5 +20,65 @@ public class CastMemberTest {
         Assertions.assertNotNull(actualMember.getCreatedAt());
         Assertions.assertNotNull(actualMember.getUpdatedAt());
         Assertions.assertEquals(actualMember.getCreatedAt(), actualMember.getUpdatedAt());
+    }
+
+    @Test
+    public void givenAnInvalidNullName_whenCallsNewMember_shouldReceiveANotification() {
+        final String expectedName = null;
+        final var expectedType = CastMemberType.ACTOR;
+        final var expectedErrorCount = 1;
+        final var expectedErrorMessage = "'name' should not be null";
+
+        final var actualException = Assertions.assertThrows(NotificationException.class,
+                () -> CastMember.newMember(expectedName, expectedType));
+
+        Assertions.assertNotNull(actualException);
+        Assertions.assertEquals(expectedErrorCount, actualException.getErrors().size());
+        Assertions.assertEquals(expectedErrorMessage, actualException.getErrors().get(0).message());
+    }
+
+    @Test
+    public void givenAnInvalidEmptyName_whenCallsNewMember_shouldReceiveANotification() {
+        final var expectedName = " ";
+        final var expectedType = CastMemberType.ACTOR;
+        final var expectedErrorCount = 1;
+        final var expectedErrorMessage = "'name' should not be empty";
+
+        final var actualException = Assertions.assertThrows(NotificationException.class,
+                () -> CastMember.newMember(expectedName, expectedType));
+
+        Assertions.assertNotNull(actualException);
+        Assertions.assertEquals(expectedErrorCount, actualException.getErrors().size());
+        Assertions.assertEquals(expectedErrorMessage, actualException.getErrors().get(0).message());
+    }
+
+    @Test
+    public void givenAnInvalidNameWithLengthMoreThan255_whenCallsNewMember_shouldReceiveANotification() {
+        final String expectedName = "'name' must be between 3 and 255 characters 'name' must be between 3 and 255 characters 'name' must be between 3 and 255 characters 'name' must be between 3 and 255 characters 'name' must be between 3 and 255 characters 'name' must be between 3 and 255 characters name' must be between 3 and 255 characters";
+        final var expectedType = CastMemberType.ACTOR;
+        final var expectedErrorCount = 1;
+        final var expectedErrorMessage = "'name' must be between 3 and 255 characters";
+
+        final var actualException = Assertions.assertThrows(NotificationException.class,
+                () -> CastMember.newMember(expectedName, expectedType));
+
+        Assertions.assertNotNull(actualException);
+        Assertions.assertEquals(expectedErrorCount, actualException.getErrors().size());
+        Assertions.assertEquals(expectedErrorMessage, actualException.getErrors().get(0).message());
+    }
+
+    @Test
+    public void givenAnInvalidNullType_whenCallsNewMember_shouldReceiveANotification() {
+        final var expectedName = "vin diesel";
+        final CastMemberType expectedType = null;
+        final var expectedErrorCount = 1;
+        final var expectedErrorMessage = "'type' should not be null";
+
+        final var actualException = Assertions.assertThrows(NotificationException.class,
+                () -> CastMember.newMember(expectedName, expectedType));
+
+        Assertions.assertNotNull(actualException);
+        Assertions.assertEquals(expectedErrorCount, actualException.getErrors().size());
+        Assertions.assertEquals(expectedErrorMessage, actualException.getErrors().get(0).message());
     }
 }
