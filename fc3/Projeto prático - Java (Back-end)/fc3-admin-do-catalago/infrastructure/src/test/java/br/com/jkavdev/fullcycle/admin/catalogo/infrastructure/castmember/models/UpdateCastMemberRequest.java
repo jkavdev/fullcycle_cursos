91749886863be9aebae935a2f3ca14d7d9d07d2a
JHONATAN KOLEN ALVES VIANA <f930649@corp.caixa.gov.br>
2023-02-18 01:33:12 -0300

    Full Cycle 3.0 - Projeto prático - Java (Back-end) - 1810 - testes de serializacao da classe castmemberresponse

--- fc3/Projeto prático - Java (Back-end)/fc3-admin-do-catalago/infrastructure/src/test/java/br/com/jkavdev/fullcycle/admin/catalogo/infrastructure/castmember/models/UpdateCastMemberRequest.java
+++
@@ -1,6 +0,0 @@
-package br.com.jkavdev.fullcycle.admin.catalogo.infrastructure.castmember.models;
-
-import br.com.jkavdev.fullcycle.admin.catalogo.domain.castmember.CastMemberType;
-
-public record UpdateCastMemberRequest(String name, CastMemberType type) {
-}
