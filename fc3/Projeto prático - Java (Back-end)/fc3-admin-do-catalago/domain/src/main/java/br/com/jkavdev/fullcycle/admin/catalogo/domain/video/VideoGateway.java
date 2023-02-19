00e5cad1b7a4351ba3a577f26f95ddfd6f164d8e
JHONATAN KOLEN ALVES VIANA <f930649@corp.caixa.gov.br>
2023-02-19 13:36:58 -0300

    Full Cycle 3.0 - Projeto prático - Java (Back-end) - 2010 - criando a interface do gateway de video

---
+++ fc3/Projeto prático - Java (Back-end)/fc3-admin-do-catalago/domain/src/main/java/br/com/jkavdev/fullcycle/admin/catalogo/domain/video/VideoGateway.java
@@ -0,0 +1,18 @@
+package br.com.jkavdev.fullcycle.admin.catalogo.domain.video;
+
+import br.com.jkavdev.fullcycle.admin.catalogo.domain.pagination.Pagination;
+
+import java.util.Optional;
+
+public interface VideoGateway {
+
+    Video create(Video aVideo);
+
+    void deleteById(VideoID anId);
+
+    Optional<Video> findById(VideoID anId);
+
+    Video update(Video aVideo);
+
+    Pagination<Video> findAll(VideoSearchQuery aQuery);
+}
---
+++ fc3/Projeto prático - Java (Back-end)/fc3-admin-do-catalago/domain/src/main/java/br/com/jkavdev/fullcycle/admin/catalogo/domain/video/VideoSearchQuery.java
@@ -0,0 +1,10 @@
+package br.com.jkavdev.fullcycle.admin.catalogo.domain.video;
+
+public record VideoSearchQuery(
+        int page,
+        int perPage,
+        String terms,
+        String sort,
+        String direction
+) {
+}
