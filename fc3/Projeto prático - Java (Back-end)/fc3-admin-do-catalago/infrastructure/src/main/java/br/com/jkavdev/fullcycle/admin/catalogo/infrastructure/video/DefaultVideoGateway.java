f52970d8549310d8a6ef7b1bfabceee9bb67dead
JHONATAN KOLEN ALVES VIANA <f930649@corp.caixa.gov.br>
2023-02-21 11:06:34 -0300

    Full Cycle 3.0 - Projeto prático - Java (Back-end) - 2213 - implementando o metodo create do gateway de video

---
+++ fc3/Projeto prático - Java (Back-end)/fc3-admin-do-catalago/infrastructure/src/main/java/br/com/jkavdev/fullcycle/admin/catalogo/infrastructure/video/DefaultVideoGateway.java
@@ -0,0 +1,49 @@
+package br.com.jkavdev.fullcycle.admin.catalogo.infrastructure.video;
+
+import br.com.jkavdev.fullcycle.admin.catalogo.domain.pagination.Pagination;
+import br.com.jkavdev.fullcycle.admin.catalogo.domain.video.Video;
+import br.com.jkavdev.fullcycle.admin.catalogo.domain.video.VideoGateway;
+import br.com.jkavdev.fullcycle.admin.catalogo.domain.video.VideoID;
+import br.com.jkavdev.fullcycle.admin.catalogo.domain.video.VideoSearchQuery;
+import br.com.jkavdev.fullcycle.admin.catalogo.infrastructure.video.persistence.VideoJpaEntity;
+import br.com.jkavdev.fullcycle.admin.catalogo.infrastructure.video.persistence.VideoRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Objects;
+import java.util.Optional;
+
+public class DefaultVideoGateway implements VideoGateway {
+
+    private final VideoRepository videoRepository;
+
+    public DefaultVideoGateway(final VideoRepository videoRepository) {
+        this.videoRepository = Objects.requireNonNull(videoRepository);
+    }
+
+    @Override
+    @Transactional
+    public Video create(Video aVideo) {
+        return this.videoRepository.save(VideoJpaEntity.from(aVideo))
+                .toAggregate();
+    }
+
+    @Override
+    public void deleteById(VideoID anId) {
+
+    }
+
+    @Override
+    public Optional<Video> findById(VideoID anId) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Video update(Video aVideo) {
+        return null;
+    }
+
+    @Override
+    public Pagination<Video> findAll(VideoSearchQuery aQuery) {
+        return null;
+    }
+}
