963a43bce8003dd3b4142593dad69132f30eab6a
JHONATAN KOLEN ALVES VIANA <f930649@corp.caixa.gov.br>
2023-02-21 02:48:17 -0300

    Full Cycle 3.0 - Projeto prático - Java (Back-end) - 2208 - criando o jpa repository de video

---
+++ fc3/Projeto prático - Java (Back-end)/fc3-admin-do-catalago/infrastructure/src/main/java/br/com/jkavdev/fullcycle/admin/catalogo/infrastructure/video/persistence/VideoRepository.java
@@ -0,0 +1,8 @@
+package br.com.jkavdev.fullcycle.admin.catalogo.infrastructure.video.persistence;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface VideoRepository extends JpaRepository<VideoJpaEntity, UUID> {
+}
