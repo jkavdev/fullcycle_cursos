f0b7130c94787c776a1a397f60b52158fde3ed00
JHONATAN KOLEN ALVES VIANA <f930649@corp.caixa.gov.br>
2023-02-25 21:39:30 -0300

    Full Cycle 3.0 - Projeto prático - Java (Back-end) - 2305 - criando a facade storage service

---
+++ fc3/Projeto prático - Java (Back-end)/fc3-admin-do-catalago/infrastructure/src/main/java/br/com/jkavdev/fullcycle/admin/catalogo/infrastructure/service/local/InMemoryStorageService.java
@@ -0,0 +1,49 @@
+package br.com.jkavdev.fullcycle.admin.catalogo.infrastructure.service.local;
+
+import br.com.jkavdev.fullcycle.admin.catalogo.domain.video.Resource;
+import br.com.jkavdev.fullcycle.admin.catalogo.infrastructure.service.StorageService;
+
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class InMemoryStorageService implements StorageService {
+
+    private final Map<String, Resource> storage;
+
+    public InMemoryStorageService() {
+        this.storage = new ConcurrentHashMap<>();
+    }
+
+    public Map<String, Resource> storage() {
+        return storage;
+    }
+
+    public void reset() {
+        this.storage.clear();
+    }
+
+    @Override
+    public void deleteAll(Collection<String> names) {
+        names.forEach(this.storage::remove);
+    }
+
+    @Override
+    public Optional<Resource> get(String name) {
+        return Optional.ofNullable(this.storage.get(name));
+    }
+
+    @Override
+    public List<String> list(String prefix) {
+        if (prefix == null) {
+            return Collections.emptyList();
+        }
+        return this.storage.keySet().stream()
+                .filter(it -> it.startsWith(prefix))
+                .toList();
+    }
+
+    @Override
+    public void store(String name, Resource resource) {
+        this.storage.put(name, resource);
+    }
+}
