d405005a1e248cc8fbd8f60e22a7b4afdb374a62
JHONATAN KOLEN ALVES VIANA <f930649@corp.caixa.gov.br>
2023-03-03 20:02:24 -0300

    Full Cycle 3.0 - Projeto prático - Java (Back-end) - 2506 - adicionando a configuracao de beans e qualificando os beans para injecao do spring do amqp

---
+++ fc3/Projeto prático - Java (Back-end)/fc3-admin-do-catalago/infrastructure/src/main/java/br/com/jkavdev/fullcycle/admin/catalogo/infrastructure/configuration/amqp/AmqpConfig.java
@@ -0,0 +1,30 @@
+package br.com.jkavdev.fullcycle.admin.catalogo.infrastructure.configuration.amqp;
+
+import br.com.jkavdev.fullcycle.admin.catalogo.infrastructure.configuration.annotations.VideoCreatedQueue;
+import br.com.jkavdev.fullcycle.admin.catalogo.infrastructure.configuration.annotations.VideoEncodedQueue;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AmqpConfig {
+
+    @Bean
+    @ConfigurationProperties("amqp.queues.video-created")
+    @VideoCreatedQueue
+    public QueueProperties videoCreatedQueueProperties() {
+        return new QueueProperties();
+    }
+
+    @Bean
+    @ConfigurationProperties("amqp.queues.video-encoded")
+    @VideoEncodedQueue
+    public QueueProperties videoEncodedQueueProperties() {
+        return new QueueProperties();
+    }
+
+    @Bean
+    public String queueName(@VideoCreatedQueue QueueProperties props) {
+        return props.getQueue();
+    }
+}
