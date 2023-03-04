b9b65096fc39f5516b1ab0944709f26e45aa998f
JHONATAN KOLEN ALVES VIANA <f930649@corp.caixa.gov.br>
2023-03-03 21:43:41 -0300

    Full Cycle 3.0 - Projeto prático - Java (Back-end) - 2507 - automatizando setup do rabbitmq com amqp admin

--- fc3/Projeto prático - Java (Back-end)/fc3-admin-do-catalago/infrastructure/src/main/java/br/com/jkavdev/fullcycle/admin/catalogo/infrastructure/configuration/amqp/AmqpConfig.java
+++ fc3/Projeto prático - Java (Back-end)/fc3-admin-do-catalago/infrastructure/src/main/java/br/com/jkavdev/fullcycle/admin/catalogo/infrastructure/configuration/amqp/AmqpConfig.java
@@ -2,6 +2,8 @@ package br.com.jkavdev.fullcycle.admin.catalogo.infrastructure.configuration.amq
 
 import br.com.jkavdev.fullcycle.admin.catalogo.infrastructure.configuration.annotations.VideoCreatedQueue;
 import br.com.jkavdev.fullcycle.admin.catalogo.infrastructure.configuration.annotations.VideoEncodedQueue;
+import br.com.jkavdev.fullcycle.admin.catalogo.infrastructure.configuration.annotations.VideoEvents;
+import org.springframework.amqp.core.*;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -23,8 +25,52 @@ public class AmqpConfig {
         return new QueueProperties();
     }
 
-    @Bean
-    public String queueName(@VideoCreatedQueue QueueProperties props) {
-        return props.getQueue();
+    /**
+     * configurando a estrutura das filas, nao eh necessario, pois apenas precisamos dos listeners e producers
+     *
+     * mas para a niver de testes e desenvolvimentos, com essa configuracao, o spring eh capaz de criar a estrutura necessaria
+     * da exchange, queues e bindings
+     * atuando como infrastructure as a code
+     */
+    @Configuration
+    static class Admin {
+
+        @Bean
+        @VideoEvents
+        public Exchange videoEventsExchange(@VideoCreatedQueue QueueProperties props) {
+            return new DirectExchange(props.getExchange());
+        }
+
+        @Bean
+        @VideoCreatedQueue
+        public Queue videoCreatedQueue(@VideoCreatedQueue QueueProperties props) {
+            return new Queue(props.getQueue());
+        }
+
+        @Bean
+        @VideoCreatedQueue
+        public Binding videoCreatedBinding(
+                @VideoEvents DirectExchange exchange,
+                @VideoCreatedQueue Queue queue,
+                @VideoCreatedQueue QueueProperties props
+        ) {
+            return BindingBuilder.bind(queue).to(exchange).with(props.getRoutingKey());
+        }
+
+        @Bean
+        @VideoEncodedQueue
+        public Queue videoEncodedQueue(@VideoEncodedQueue QueueProperties props) {
+            return new Queue(props.getQueue());
+        }
+
+        @Bean
+        @VideoEncodedQueue
+        public Binding videoEncodedBinding(
+                @VideoEvents DirectExchange exchange,
+                @VideoEncodedQueue Queue queue,
+                @VideoEncodedQueue QueueProperties props
+        ) {
+            return BindingBuilder.bind(queue).to(exchange).with(props.getRoutingKey());
+        }
     }
 }
