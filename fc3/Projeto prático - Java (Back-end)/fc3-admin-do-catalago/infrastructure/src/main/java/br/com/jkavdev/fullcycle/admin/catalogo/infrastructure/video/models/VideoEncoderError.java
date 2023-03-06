0ba234a3b90797b57f9c0df08016b71e5b1994c5
JHONATAN KOLEN ALVES VIANA <f930649@corp.caixa.gov.br>
2023-03-05 22:53:49 -0300

    Full Cycle 3.0 - Projeto prático - Java (Back-end) - 2602 - criando as models do listener de encoder result

---
+++ fc3/Projeto prático - Java (Back-end)/fc3-admin-do-catalago/infrastructure/src/main/java/br/com/jkavdev/fullcycle/admin/catalogo/infrastructure/video/models/VideoEncoderError.java
@@ -0,0 +1,16 @@
+package br.com.jkavdev.fullcycle.admin.catalogo.infrastructure.video.models;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public record VideoEncoderError(
+        @JsonProperty("message") VideoMessage message,
+        @JsonProperty("error") String error
+) implements VideoEncoderResult {
+
+    private static final String ERROR = "ERROR";
+
+    @Override
+    public String getStatus() {
+        return ERROR;
+    }
+}
