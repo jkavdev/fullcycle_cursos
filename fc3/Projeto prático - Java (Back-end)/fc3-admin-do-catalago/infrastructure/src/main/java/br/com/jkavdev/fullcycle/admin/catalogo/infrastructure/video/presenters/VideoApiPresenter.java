67683928513cbdcfac16bcb76ea196828ea317aa
JHONATAN KOLEN ALVES VIANA <f930649@corp.caixa.gov.br>
2023-03-08 01:37:20 -0300

    Full Cycle 3.0 - Projeto prático - Java (Back-end) - 2707 - implementando o endpoint de get video por id

---
+++ fc3/Projeto prático - Java (Back-end)/fc3-admin-do-catalago/infrastructure/src/main/java/br/com/jkavdev/fullcycle/admin/catalogo/infrastructure/video/presenters/VideoApiPresenter.java
@@ -0,0 +1,62 @@
+package br.com.jkavdev.fullcycle.admin.catalogo.infrastructure.video.presenters;
+
+import br.com.jkavdev.fullcycle.admin.catalogo.application.video.retrieve.get.VideoOutput;
+import br.com.jkavdev.fullcycle.admin.catalogo.domain.video.AudioVideoMedia;
+import br.com.jkavdev.fullcycle.admin.catalogo.domain.video.ImageMedia;
+import br.com.jkavdev.fullcycle.admin.catalogo.infrastructure.video.models.AudioVideoMediaResponse;
+import br.com.jkavdev.fullcycle.admin.catalogo.infrastructure.video.models.ImageMediaResponse;
+import br.com.jkavdev.fullcycle.admin.catalogo.infrastructure.video.models.VideoResponse;
+
+public interface VideoApiPresenter {
+
+    static VideoResponse present(final VideoOutput output) {
+        return new VideoResponse(
+                output.id(),
+                output.title(),
+                output.description(),
+                output.duration(),
+                output.launchedAt(),
+                output.opened(),
+                output.published(),
+                output.rating().getName(),
+                output.createdAt(),
+                output.updatedAt(),
+                present(output.banner()),
+                present(output.thumbnail()),
+                present(output.thumbnailHalf()),
+                present(output.video()),
+                present(output.trailer()),
+                output.members(),
+                output.categories(),
+                output.genres()
+        );
+    }
+
+    static ImageMediaResponse present(final ImageMedia image) {
+        if (image == null) {
+            return null;
+        }
+
+        return new ImageMediaResponse(
+                image.id(),
+                image.checksum(),
+                image.name(),
+                image.location()
+        );
+    }
+
+    static AudioVideoMediaResponse present(final AudioVideoMedia video) {
+        if (video == null) {
+            return null;
+        }
+
+        return new AudioVideoMediaResponse(
+                video.id(),
+                video.checksum(),
+                video.name(),
+                video.rawLocation(),
+                video.encodedLocation(),
+                video.status().name()
+        );
+    }
+}
