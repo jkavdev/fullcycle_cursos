aceb39955f165b3825d97bfe0139270deaa43579
jkavdev <jhonatankolen@gmail.com>
2023-06-13 00:26:55 -0300

    Full Cycle 3.0 - Projeto prático - Java (Back-end) - 3105 - build e execucao da aplicacao junto do docker compose
    - adicionando variaveis de ambiente no profile de sandbox
    
    - criando docker compose diferentes para a aplicacao e para os servicos
    
    - alterando run.sh para criar uma rede docker externa, e executar o docker compose dos servicos

--- fc3/Projeto prático - Java (Back-end)/fc3-admin-do-catalago/sandbox/run.sh
+++ fc3/Projeto prático - Java (Back-end)/fc3-admin-do-catalago/sandbox/run.sh
@@ -1,9 +1,12 @@
+# Criar as docker networks
+docker network create adm_videos_services
+
 # Criar as pastas com permissões
 mkdir -m 777 .docker
 mkdir -m 777 .docker/keycloak
 mkdir -m 777 .docker/mysql
 
-docker compose -f app/docker-compose.yml up -d
+docker compose -f services/docker-compose.yml up -d
 
 echo "Inicializando os containers..."
 sleep 10
