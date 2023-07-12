e38b91e1e35706d93b95ab82344f62968f94bf8e
jkavdev <jhonatankolen@gmail.com>
2023-07-11 23:40:24 -0300

    Full Cycle 3.0 - Microsserviço - API do Catálogo com Java (Backend-end) - 0405 - configuracao de testes integrados do graphql

---
+++ fc3/Microsserviço - API do Catálogo com Java (Backend-end)/fc3-catalago/infrastructure/src/test/java/br/com/jkavdev/fullcycle/catalogo/infrastructure/GraphQLControllerTest.java
@@ -0,0 +1,24 @@
+package br.com.jkavdev.fullcycle.catalogo.infrastructure;
+
+import br.com.jkavdev.fullcycle.catalogo.infrastructure.configuration.ObjectMapperConfig;
+import org.junit.jupiter.api.Tag;
+import org.springframework.boot.test.autoconfigure.graphql.GraphQlTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.core.annotation.AliasFor;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.lang.annotation.*;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Inherited
+@ActiveProfiles("test-integration")
+@GraphQlTest
+@Import(ObjectMapperConfig.class)
+@Tag("integrationTests")
+public @interface GraphQLControllerTest {
+
+    @AliasFor(annotation = GraphQlTest.class, attribute = "controllers")
+    Class<?>[] controllers() default {};
+}
